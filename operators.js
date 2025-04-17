152001643168157d0d342fd4332d3eeeb585a8fc
Sritejaswini123 <Sritejaswini1234@gmail.com>
2025-04-17 17:15:01 +0530

    CallBack

--- operators.js
+++ operators.js
@@ -80,3 +80,10 @@ console.log(b>=c)
 console.log(b<=c)
 console.log(b==c)
 console.log(b!=c)
+
+
+
+//Spread and Rest operators
+let arr1 = [1,2,3,4,5];
+let arr2 = [...arr1,6,7,8,9,10];
+console.log(arr2);
